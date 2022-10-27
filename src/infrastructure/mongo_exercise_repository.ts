29def0154a20676f55358928339bc9c5b2b62f46
Andreas <akbr18@student.aau.dk>
2022-10-27 08:53:46 +0200

    fix: Mongo repository exercises now have constant IDs

--- src/infrastructure/mongo_exercise_repository.ts
+++ src/infrastructure/mongo_exercise_repository.ts
@@ -2,20 +2,33 @@ import { v4 as uuidv4 } from 'uuid';
 import { Exercise, IExerciseRepository } from '../domain';
 
 export class MongoExerciseRepository implements IExerciseRepository {
-  private readonly exercises: Exercise[] = [
-    new Exercise(uuidv4(), 'Hello, World!', 'This exrcise will cover...', []),
-    new Exercise(uuidv4(), 'Doom', 'This is brutal...', []),
-  ];
+  private readonly exercises: Exercise[] = [];
+
+  constructor() {
+    this.exercises = [
+      new Exercise('2ff1b6ba-5823-4551-9827-0d669da4b6fd', 'Hello, World!', 'This exrcise will cover...', []),
+      new Exercise('7ad52ef0-34f0-4d59-b9ff-94eca3ca7558', 'Doom', 'This is brutal...', []),
+    ]
+  }
 
   public async getExercises(): Promise<Exercise[]> {
     return this.exercises;
   }
 
   public async getExerciseById(id: string): Promise<Exercise> {
-    const exercise = this.exercises.find((value) => value.id == id);
+    console.log(this.exercises);
+    const exercise = this.exercises.find((value) => {
+      console.log(value);
+      return value.id == id
+    });
     if (exercise === undefined) {
       Promise.reject('Exercise not found');
     }
-    return Promise.resolve(exercise!);
+
+    if (exercise == undefined) {
+      return Promise.reject()
+    } else {
+      return Promise.resolve(exercise)
+    }
   }
 }
