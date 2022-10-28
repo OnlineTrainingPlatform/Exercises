f05a05e43b00ffde4313ac9cdf66f772d00840ca
Andreas <akbr18@student.aau.dk>
2022-10-28 08:14:58 +0200

    chore: Added mongoose

--- src/infrastructure/mongo_exercise_repository.ts
+++ src/infrastructure/mongo_exercise_repository.ts
@@ -1,6 +1,17 @@
 import { Exercise, IExerciseRepository } from '../domain';
+import { Schema, model, connect, Model, Mongoose } from 'mongoose';
+
+interface IExerciseDocument {
+  id: string,
+  title: string,
+  description: string
+}
 
 export class MongoExerciseRepository implements IExerciseRepository {
+  private readonly exerciseSchmea: Schema<IExerciseDocument>;
+  private readonly exerciseModel: Model<IExerciseDocument>;
+  private mongoose: Mongoose | undefined = undefined;
+
   private readonly exercises: Exercise[] = [];
 
   constructor() {
@@ -18,13 +29,52 @@ export class MongoExerciseRepository implements IExerciseRepository {
         [],
       ),
     ];
+
+    this.exerciseSchmea = new Schema<IExerciseDocument>({
+      id: { type: String, required: true },
+      title: { type: String, required: true },
+      description: { type: String, required: true }
+    });
+
+    this.exerciseModel = model<IExerciseDocument>('Exercise', this.exerciseSchmea);
+  }
+
+  private convertModel(model: IExerciseDocument): Exercise {
+    return new Exercise(model.id, model.title, model.description, [])
+  }
+
+  private convertModels(models: IExerciseDocument[]): Exercise[] {
+    const exercises = []
+    for (const model of models) {
+      exercises.push(this.convertModel(model));
+    }
+    return exercises;
+  }
+
+  private async connect(): Promise<Mongoose> {
+    if (this.mongoose != undefined) {
+      return Promise.resolve(this.mongoose);
+    }
+
+    return connect('mongodb://root:eduroam@localhost:27017/?authMechanism=DEFAULT', {
+      dbName: 'exercises',
+      autoCreate: true
+    });
   }
 
   public async getExercises(): Promise<Exercise[]> {
-    return this.exercises;
+    await this.connect();
+
+    const models = await this.exerciseModel.find({})
+    if (models == null) {
+      return Promise.reject(undefined);
+    }
+
+    return this.convertModels(models);
   }
 
   public async getExerciseById(id: string): Promise<Exercise | undefined> {
+    await this.connect();
     const exercise = this.exercises.find((value) => {
       return value.id == id;
     });
